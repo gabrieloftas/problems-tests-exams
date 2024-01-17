288e194d7a3eccc687de42eb2965789ee87d3c73
Gabriel Oliveira de Freitas <gabrieloftas@edu.unifor.br>
2024-01-17 19:09:27 -0300

    Prova algoritmos e variáveis

---
+++ InfinitySchool/ProvaAlgoritmosVariaveis.py
@@ -0,0 +1,7 @@
+#inteiros,booleanos,strings,floats
+inteiro=20
+booleano=True
+string="Texto Genérico"
+float=3.1416
+solicitacaoDeDados=input("Digite qualquer coisa:")
+print(f"Você digitou {solicitacaoDeDados}")
